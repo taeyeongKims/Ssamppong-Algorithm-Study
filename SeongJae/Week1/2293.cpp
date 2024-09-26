4bbefee424b61c9182510712eca7c07a30d1308f
seonna7 <seongjae3588@gmail.com>
2024-09-26 21:23:28 +0900

    문제 풀이

--- SeongJae/Week1/2293.cpp
+++ SeongJae/Week1/2293.cpp
@@ -1,7 +1,31 @@
 #include <iostream>
+#include <vector>
 using namespace std;
+
+int CoinCnt;
+int TotalVal;
 
 int main()
 {
-	return 1;
+	cin >> CoinCnt >> TotalVal;
+
+	int* CoinVal = new int[CoinCnt];
+	int DP[100000] = { 0, };
+	for (int i = 0; i < CoinCnt; i++)
+	{
+		int rep;	cin >> rep;
+		CoinVal[i] = rep;
+	}
+	DP[0] = 1;
+
+	for (int i = 0; i < CoinCnt; i++)
+	{
+		for (int j = 1; j <= TotalVal; j++)
+		{
+			if (j - CoinVal[i] >= 0)
+				DP[j] = DP[j] + DP[j - CoinVal[i]];
+		}
+	}
+	cout << DP[TotalVal] << endl;
+	return 0;
 }
